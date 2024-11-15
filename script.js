8de9c316f5b3be8bbd0cb83604a6d5735f7b0b66
Jewlun <julian.boehnertkrueger@gmail.com>
2024-11-15 02:43:02 -0500

    Got all pages linked together, got elements formated and placed logo

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("indexbtn-1").onclick = function() {
-    location.href = "2newUserPage.html"
-}
-
-document.getElementById("indexbtn-2").onclick = function() {
-    location.href = "3userLoginPage.html"
-}
