ddcb2aaffe8a590805f950c26f6f7aa5f4ac17ee
Jewlun <julian.boehnertkrueger@gmail.com>
2024-11-14 21:25:00 -0500

    Added buttons and javascript, linking all of the pages together

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("indexbtn-1").onclick = function() {
+    location.href = "2newUserPage.html"
+}
+
+document.getElementById("indexbtn-2").onclick = function() {
+    location.href = "3userLoginPage.html"
+}
